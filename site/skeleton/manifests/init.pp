30fbb9f355f75308cc738268fb6d91d56e3b676c
tkalligo <tim_kalligonis@cable.comcast.com>
2016-02-18 15:09:05 -0500

    Lab 9.3

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,10 @@
+class skeleton{
+  file { "/etc/skel" :
+    ensure => directory,
+  }
+  
+  file { "/etc/skel/.bashrc" :
+    ensure => file,
+    source => "puppet:///modules/skeleton/bashrc",
+  }
+}
